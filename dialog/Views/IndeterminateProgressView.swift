1e7b2fa016328900bde4b0085c89e26f6e237314
Bart Reardon <bart.reardon@csiro.au>
2025-09-12 23:19:46 +1000

    Clean up indeterminate progress view

--- dialog/Views/IndeterminateProgressView.swift
+++ dialog/Views/IndeterminateProgressView.swift
@@ -8,64 +8,33 @@
 import SwiftUI
 
 // This is required as macOS 26 broke the indeterminate progress view animation
-// source https://matthewcodes.uk/articles/indeterminate-linear-progress-view/
+// idea source https://matthewcodes.uk/articles/indeterminate-linear-progress-view/
 
 struct IndeterminateProgressView: View {
-    @State private var width: CGFloat = 0
     @State private var offset: CGFloat = 0
-    @Environment(\.isEnabled) private var isEnabled
 
     var body: some View {
-        Rectangle()
-            .foregroundColor(.gray.opacity(0.15))
-            .readWidth()
-            .overlay(
+        GeometryReader { geometry in
                 Rectangle()
-                    .foregroundColor(Color.accentColor)
-                    .frame(width: self.width * 0.26, height: 8)
+                    .foregroundColor(.gray.opacity(0.15))
+                    .overlay(
+                        Rectangle()
+                            .fill(LinearGradient(colors: [.clear, .accentColor, .clear], startPoint: .leading, endPoint: .trailing))
+                            .frame(width: geometry.size.width * 0.25, height: 8)
+                            .clipShape(Capsule())
+                            .offset(x: -geometry.size.width * 0.6, y: 0)
+                            .offset(x: geometry.size.width * 1.2 * self.offset, y: 0)
+                            .animation(.easeInOut.repeatForever().speed(0.25), value: self.offset)
+                            .onAppear {
+                                withAnimation {
+                                    self.offset = 1
+                                }
+                            }
+                    )
                     .clipShape(Capsule())
-                    .offset(x: -self.width * 0.6, y: 0)
-                    .offset(x: self.width * 1.2 * self.offset, y: 0)
-                    .animation(.easeInOut.repeatForever().speed(0.25), value: self.offset)
-                    .onAppear {
-                        withAnimation {
-                            self.offset = 1
-                        }
-                    }
-            )
-            .clipShape(Capsule())
-            .opacity(self.isEnabled ? 1 : 0)
-            .animation(.default, value: self.isEnabled)
-            .frame(height: 8)
-            .onPreferenceChange(WidthPreferenceKey.self) { width in
-                self.width = width
+                    .frame(height: 8)
+                    .padding(.top, 6)
             }
     }
 }
 
-struct WidthPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = max(value, nextValue())
-    }
-}
-
-private struct ReadWidthModifier: ViewModifier {
-    private var sizeView: some View {
-        GeometryReader { geometry in
-            Color.clear.preference(key: WidthPreferenceKey.self, value: geometry.size.width)
-        }
-    }
-
-    func body(content: Content) -> some View {
-        content.background(sizeView)
-    }
-}
-
-extension View {
-    func readWidth() -> some View {
-        self
-            .modifier(ReadWidthModifier())
-    }
-}
